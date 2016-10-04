6d849765b7600cac409621eff95920d0ef19324c
dm1try <me@dmitry.it>
2016-10-04 20:26:16 +0300

    change default values for quick fix column to proper one

--- rplugin/elixir/apps/ilexir/lib/ilexir/quick_fix/item.ex
+++ rplugin/elixir/apps/ilexir/lib/ilexir/quick_fix/item.ex
@@ -2,6 +2,6 @@ defmodule Ilexir.QuickFix.Item do
   defstruct [:file, :text, :type, :location]
 
   defmodule Location do
-    defstruct [line: 1, col_start: 1,  col_end: 1]
+    defstruct [line: 1, col_start: 0,  col_end: -1]
   end
 end
