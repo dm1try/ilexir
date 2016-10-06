e23ff7e0be8509cfaa53465598c9190f4fe0599a
dm1try <me@dmitry.it>
2016-10-06 17:52:08 +0300

    include "hosted" path to compiler paths in test env

--- rplugin/elixir/apps/ilexir/mix.exs
+++ rplugin/elixir/apps/ilexir/mix.exs
@@ -9,12 +9,17 @@ defmodule Ilexir.Mixfile do
      lockfile: "../../mix.lock",
      preferred_cli_env: [espec: :test],
      elixir: "~> 1.3",
+     elixirc_paths: elixirc_paths(),
      deps: deps()]
   end
 
   def application do
     options = [applications: [:logger] , env: [plugin_module: Ilexir.Plugin]]
     if Mix.env != :test, do: options ++ [mod: {Ilexir, []}], else: options
+  end
+
+  def elixirc_paths do
+    if Mix.env != :test, do: ["lib"], else: ["lib", "hosted"]
   end
 
   defp deps do
