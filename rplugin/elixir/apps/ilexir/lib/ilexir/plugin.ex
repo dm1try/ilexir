f9fb948f72b768b4b29db6924caff592dffebff2
dm1try <me@dmitry.it>
2016-10-07 13:54:57 +0300

    add compile/evaluate nvim commands closes #6

--- rplugin/elixir/apps/ilexir/lib/ilexir/plugin.ex
+++ rplugin/elixir/apps/ilexir/lib/ilexir/plugin.ex
@@ -5,8 +5,10 @@ defmodule Ilexir.Plugin do
   require Logger
 
   alias Ilexir.HostAppManager, as: AppManager
+  alias Ilexir.HostApp, as: App
   alias Ilexir.Linter
 
+  # Host manager interface
   command ilexir_start_app(path) do
     response_to_vim = case Ilexir.HostAppManager.start_app(path) do
       {:ok, _app} ->
@@ -19,9 +21,40 @@ defmodule Ilexir.Plugin do
   end
 
   command ilexir_running_apps do
-    apps = Ilexir.HostAppManager.running_apps()
+    apps = AppManager.running_apps()
 
     vim_command "echo '#{inspect apps}'"
+  end
+
+  # Compiler interface
+
+  command ilexir_compile do
+    with {:ok, buffer} <- vim_get_current_buffer,
+         {:ok, lines} <- nvim_buf_get_lines(buffer, 0, -1, false),
+         {:ok, filename} <- nvim_buf_get_name(buffer),
+         {:ok, app} <- AppManager.lookup(filename) do
+
+      content = Enum.join(lines, "\n")
+      App.compile_string(app, content, filename)
+    else
+      error ->
+        warning_with_echo("Unable to compile the file: #{inspect error}")
+    end
+  end
+
+  command ilexir_eval, range: true do
+    with {:ok, buffer} <- vim_get_current_buffer,
+         {:ok, lines} <- nvim_buf_get_lines(buffer, range_start - 1, range_end, false),
+         {:ok, filename} <- nvim_buf_get_name(buffer),
+         {:ok, app} <- AppManager.lookup(filename) do
+
+      content = Enum.join(lines, "\n")
+
+      evaluate_with_undefined(app, content, filename, range_start)
+    else
+      error ->
+        warning_with_echo("Unable to evaluate lines: #{inspect error}")
+    end
   end
 
   on_event :insert_leave, [pattern: "*.{ex,exs}"], do: lint(Linter.Ast)
@@ -40,5 +73,29 @@ defmodule Ilexir.Plugin do
       error ->
         Logger.warn("Unable to lint the buffer: #{inspect error}")
     end
+  end
+
+  defp evaluate_with_undefined(app, content, filename, line) do
+    case App.eval_string(app, content, filename, line) do
+      {:ok, result} -> echo_i(result)
+      {:undefined, var} ->
+        {:ok, result} = nvim_call_function("input", ["Please provide '#{var}' to continue: "])
+        App.eval_string(app, "#{var} = #{result}", filename, line)
+        evaluate_with_undefined(app, content, filename, line)
+      {:error, error} -> echo_i(error)
+    end
+  end
+
+  defp warning_with_echo(message) do
+    Logger.warn(message)
+    echo(message)
+  end
+
+  def echo(param) do
+    vim_command "echo '#{param}'"
+  end
+
+  def echo_i(param) do
+    vim_command "echo '#{inspect param, pretty: true}'"
   end
 end
