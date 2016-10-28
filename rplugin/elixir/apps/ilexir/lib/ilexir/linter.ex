9069a306248563960bd7d4058c4d738f6b3e0d4f
dm1try <me@dmitry.it>
2016-10-28 09:39:48 +0300

    run linters that depend on current settings/app

--- rplugin/elixir/apps/ilexir/lib/ilexir/linter.ex
+++ rplugin/elixir/apps/ilexir/lib/ilexir/linter.ex
@@ -1,4 +1,5 @@
 defmodule Ilexir.Linter do
+  @moduledoc false
   use GenServer
   require Logger
 
@@ -8,49 +9,82 @@ defmodule Ilexir.Linter do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
-  def check(file, content, linter, app) do
-    GenServer.call(__MODULE__, {:check, file, content, linter, app})
+  def init(_args) do
+    {:ok, %{apps_with_credo_ids: []}}
   end
 
-  def handle_call({:check, file, content, linter, app}, _from, state) do
-    case HostApp.call(app, linter, :run, [file, content]) do
-      fix_items when is_list(fix_items) ->
-       QuickFix.update_items(fix_items, to_string(linter))
-      {:error, error} ->
-        Logger.error "Problem with running a linter(#{linter}): #{inspect error}"
-    end
+  def check(file, content, linter_or_opts, app) do
+    GenServer.call(__MODULE__, {:check, file, content, linter_or_opts, app})
+  end
+
+  def handle_call({:check, file, content, linter, app}, _from, state) when is_atom(linter) do
+    run_linter(app, linter, file, content)
+    {:reply, :ok, state}
+  end
+
+  def handle_call({:check, file, content, opts, %{id: app_id} = app}, _from, state) when is_list(opts) do
+    has_credo? = Enum.any?(state.apps_with_credo_ids, &(&1 == app_id))
+    allow_compile? = Keyword.get(opts, :allow_compile, false)
+
+    linters = find_suitable_linters(has_credo?, allow_compile?)
+    Enum.each(linters, &run_linter(app, &1, file, content))
 
     {:reply, :ok, state}
   end
 
-  def handle_call({:on_app_load, app}, _from, state) do
+  def handle_call({:on_app_load, app}, _from, %{apps_with_credo_ids: apps_with_credo_ids} = state) do
     workers = bootstrap_app(app)
+
+    state = if app_has_credo?(app) do
+      bootstrap_credo(app)
+      ids = [app.id | apps_with_credo_ids]
+      %{state | apps_with_credo_ids: ids}
+    else
+      state
+    end
 
     {:reply, {:ok, workers}, state}
   end
 
-  defp bootstrap_app(app) do
-    Ilexir.HostApp.call(app, Code, :load_file, ["#{__DIR__}/quick_fix/item.ex"])
+  defp find_suitable_linters(has_credo?, allow_compile?) do
+    alias Ilexir.Linter.{Credo, Compiler, Ast}
 
-    Ilexir.HostApp.load_hosted_file(app, "ilexir/linter/dummy.ex")
-    Ilexir.HostApp.load_hosted_file(app, "ilexir/linter/ast.ex")
-
-    Ilexir.HostApp.load_hosted_file(app, "ilexir/linter/compiler.ex")
-    Ilexir.HostApp.load_hosted_file(app, "ilexir/standard_error_stub.ex")
-    bootstrap_credo(app)
-
-    [Supervisor.Spec.worker(Ilexir.StandardErrorStub,[[],[name: Ilexir.StandardErrorStub]])]
-  end
-
-  defp bootstrap_credo(%{mix_app?: true} = app) do
-    config = HostApp.call(app, Mix.Project, :config, [])
-
-    if Enum.any?(config[:deps], fn(dep)-> elem(dep, 0) == :credo end) do
-      HostApp.call(app, Application, :ensure_all_started, [:credo])
-      HostApp.load_hosted_file(app, "ilexir/linter/credo.ex")
+    cond do
+      has_credo? && allow_compile? -> [Credo, Compiler]
+      !has_credo? && allow_compile? -> [Compiler]
+      has_credo? && !allow_compile? -> [Credo]
+      true -> [Ast]
     end
   end
 
-  defp bootstrap_credo(_app), do: nil
+  defp run_linter(app, linter, file, content) do
+    case HostApp.call(app, linter, :run, [file, content]) do
+      fix_items when is_list(fix_items) ->
+        QuickFix.update_items(fix_items, to_string(linter))
+      {:error, error} ->
+        Logger.error("Problem with running a linter: #{inspect error}")
+    end
+  end
+
+  defp bootstrap_app(app) do
+    HostApp.call(app, Code, :load_file, ["#{__DIR__}/quick_fix/item.ex"])
+
+    HostApp.load_hosted_file(app, "ilexir/linter/ast.ex")
+    HostApp.load_hosted_file(app, "ilexir/linter/compiler.ex")
+    HostApp.load_hosted_file(app, "ilexir/standard_error_stub.ex")
+
+    [Supervisor.Spec.worker(Ilexir.StandardErrorStub,[[],[]])]
+  end
+
+  defp app_has_credo?(%{mix_app?: false}), do: false
+  defp app_has_credo?(%{mix_app?: true} = app) do
+    config = HostApp.call(app, Mix.Project, :config, [])
+    Enum.any?(config[:deps], fn(dep)-> elem(dep, 0) == :credo end)
+  end
+
+  defp bootstrap_credo(app) do
+    HostApp.call(app, Application, :ensure_all_started, [:credo])
+    HostApp.load_hosted_file(app, "ilexir/linter/credo.ex")
+  end
 end
 
