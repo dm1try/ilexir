ec913a82bbef0471bb4c6545a2a9258bf1c4bfc8
dm1try <me@dmitry.it>
2016-11-20 03:35:23 +0300

    match on objects instead of check module type each time

--- rplugin/elixir/apps/ilexir/hosted/ilexir/object_source/web.ex
+++ rplugin/elixir/apps/ilexir/hosted/ilexir/object_source/web.ex
@@ -1,43 +1,36 @@
 defmodule Ilexir.ObjectSource.Web do
   @moduledoc false
   alias Ilexir.ObjectSource, as: OS
-
-  alias Ilexir.Code
   import Ilexir.Code.Info, only: [compile_info: 1]
 
   @spec docs_url(OS.source_object) :: String.t | nil
-
   @doc "Finds online docs for the given object"
-  def docs_url({:module, mod} = object) do
-    if Code.elixir_module?(mod) do
-      elixir_url(object)
-    end
-  end
-
-  def docs_url({:function, {mod, {func_name, func_arity}}} = _object) do
-    if Code.elixir_module?(mod) do
-      module_url = elixir_url({:module, mod})
-      func_anchor = "##{func_name}/#{func_arity}"
-      "#{module_url}#{func_anchor}"
-    end
-  end
-
-  def docs_url({:function, {mod, _func_name}} = _object) do
-    if Code.elixir_module?(mod) do
-      elixir_url({:module, mod})
-    end
-  end
-
-  def docs_url(_), do: nil
-
-  defp elixir_url({:module, mod}) do
+  def docs_url({:module, mod}) do
     with app when is_atom(app) <- find_mod_app(mod),
          version when is_bitstring(version) <- find_app_version(app) do
 
       base_url = Hex.Utils.hexdocs_url(app, version)
       "#{base_url}/#{inspect(mod)}.html"
+    else
+      _ -> nil
     end
   end
+
+  def docs_url({:function, {mod, {func_name, func_arity}}}) do
+    case docs_url({:module, mod}) do
+      module_url when is_bitstring(module_url) ->
+        func_anchor = "##{func_name}/#{func_arity}"
+        "#{module_url}#{func_anchor}"
+
+      _ -> nil
+    end
+  end
+
+  def docs_url({:function, {mod, _func_name}}) do
+    docs_url({:module, mod})
+  end
+
+  def docs_url(_), do: nil
 
   defp find_mod_app(mod) do
     source = compile_info(mod)[:source] |> to_string
