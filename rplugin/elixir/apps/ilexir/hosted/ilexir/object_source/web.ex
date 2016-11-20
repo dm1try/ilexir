7ccdc1ff4e40d1f42c393faa945d56f4283296ae
dm1try <me@dmitry.it>
2016-11-20 11:57:42 +0300

    show online docs for elixir and erlang core applications - elixir resolver respects current version - erlang resolver uses docs for the latest stable OTP

--- rplugin/elixir/apps/ilexir/hosted/ilexir/object_source/web.ex
+++ rplugin/elixir/apps/ilexir/hosted/ilexir/object_source/web.ex
@@ -3,16 +3,25 @@ defmodule Ilexir.ObjectSource.Web do
   alias Ilexir.ObjectSource, as: OS
   import Ilexir.Code.Info, only: [compile_info: 1]
 
+  @elixir_docs_base_url "http://elixir-lang.org/docs"
+  @erlang_docs_base_url "http://erlang.org/doc/man"
+
   @spec docs_url(OS.source_object) :: String.t | nil
   @doc "Finds online docs for the given object"
   def docs_url({:module, mod}) do
-    with app when is_atom(app) <- find_mod_app(mod),
-         version when is_bitstring(version) <- find_app_version(app) do
+    with {type, app} <- find_mod_app(mod),
+         version when not is_nil(version) <- app_version(app) do
 
-      base_url = Hex.Utils.hexdocs_url(app, version)
-      "#{base_url}/#{inspect(mod)}.html"
-    else
-      _ -> nil
+      mod_name = inspect(mod)
+
+      case type do
+        :package ->
+          hex_docs_base_url = Hex.Utils.hexdocs_url(app, version)
+          "#{hex_docs_base_url}/#{mod_name}.html"
+        :core ->
+          version_without_patch = String.replace(version, ~r/\.\d+\z/, "")
+          "#{@elixir_docs_base_url}/v#{version_without_patch}/#{app}/#{mod_name}.html"
+      end
     end
   end
 
@@ -30,19 +39,57 @@ defmodule Ilexir.ObjectSource.Web do
     docs_url({:module, mod})
   end
 
-  def docs_url(_), do: nil
+  def docs_url({:erlang_module, mod}) do
+    with {type, app} <- find_erlang_app(mod) do
+      case type do
+        :stdlib -> "#{@erlang_docs_base_url}/#{app}.html"
+      end
+    end
+  end
 
-  defp find_mod_app(mod) do
-    source = compile_info(mod)[:source] |> to_string
-
-    case Regex.run(~r/deps\/(\w+)\/lib/, source) do
-      [_, app_name] -> String.to_atom(app_name)
+  def docs_url({:erlang_function, {mod, {func_name, func_arity}}}) do
+    case docs_url({:erlang_module, mod}) do
+      app_url when is_bitstring(app_url) ->
+        func_anchor = "##{func_name}-#{func_arity}"
+        "#{app_url}#{func_anchor}"
       _ -> nil
     end
   end
 
-  defp find_app_version(app) do
-    Application.spec(app)[:vsn] |> to_string
+  def docs_url(_), do: nil
+
+  defp find_mod_app(mod) do
+    source = mod_source(mod)
+
+    case Regex.run(~r/deps\/(\w+)\/lib/, source) do
+      [_, app_name] -> {:package, String.to_atom(app_name)}
+      _ -> case Regex.run(~r/lib\/(\w+)\/lib/, source) do
+        [_, core_app_name] -> {:core, String.to_atom(core_app_name)}
+        _ -> nil
+      end
+    end
+  end
+
+  defp find_erlang_app(mod) do
+    case compile_info(mod) do
+      [] -> {:stdlib, :erlang}
+      info when is_list(info) ->
+        source = to_string(info[:source])
+        case Regex.run(~r/stdlib\/src\/(\w+)\.erl/, source) do
+          [_, app_name] -> {:stdlib, String.to_atom(app_name)}
+          _ -> nil
+        end
+    end
+  end
+
+  defp mod_source(mod) do
+    compile_info(mod)[:source] |> to_string
+  end
+
+  defp app_version(app) do
+    if version = Application.spec(app, :vsn) do
+      to_string(version)
+    end
   end
 end
 
--- rplugin/elixir/apps/ilexir/spec/hosted/object_source/web_spec.exs
+++ rplugin/elixir/apps/ilexir/spec/hosted/object_source/web_spec.exs
@@ -2,9 +2,34 @@ defmodule Ilexir.ObjectSource.WebSpec do
   use ESpec
   import Ilexir.ObjectSource.Web
 
-  context "elixir module docs" do
+  context "elixir core docs" do
+    let :source, do: {:module, Enum}
+
+    let :elixir_version, do: "1.3"
+    let :expected_url, do: "http://elixir-lang.org/docs/v#{elixir_version}/elixir/Enum.html"
+
+    before do
+      allow(Application).to accept(:spec, fn(:elixir, :vsn)-> '1.3.4' end)
+      :ok
+    end
+
+    it "returns docs url for the module" do
+      expect(docs_url(source)).to eq(expected_url)
+    end
+
+    context "function docs" do
+      let :source, do: {:function, {Enum, {:any, 2}}}
+      let :expected_url, do: "http://elixir-lang.org/docs/v#{elixir_version}/elixir/Enum.html#any/2"
+
+      it "returns docs url for the function" do
+        expect(docs_url(source)).to eq(expected_url)
+      end
+    end
+  end
+
+  context "elixir external package module docs" do
     let :source, do: {:module, ESpec}
-    let :module_version, do: Application.spec(:espec)[:vsn] |> to_string
+    let :module_version, do: Application.spec(:espec, :vsn)
     let :expected_url, do: "https://hexdocs.pm/espec/#{module_version}/ESpec.html"
 
     it "returns hexdocs.pm url for elixir module" do
@@ -26,6 +51,33 @@ defmodule Ilexir.ObjectSource.WebSpec do
         it "ignores function and returns hex.pm url for module" do
           expect(docs_url(source)).to eq(expected_url)
         end
+      end
+    end
+  end
+
+  context "erlang core docs" do
+    let :source, do: {:erlang_module, :timer}
+    let :expected_url, do: "http://erlang.org/doc/man/timer.html"
+
+    it "returns docs url for the module" do
+      expect(docs_url(source)).to eq(expected_url)
+    end
+
+    context "function docs" do
+      let :source, do: {:erlang_function, {:timer, {:send_after, 2}}}
+      let :expected_url, do: "http://erlang.org/doc/man/timer.html#send_after-2"
+
+      it "returns docs url for the function" do
+        expect(docs_url(source)).to eq(expected_url)
+      end
+    end
+
+    context "mod without compile info" do
+      let :source, do: {:erlang_function, {:erlang, {:unique_integer, 0}}}
+      let :expected_url, do: "http://erlang.org/doc/man/erlang.html#unique_integer-0"
+
+      it "returns docs url for the function" do
+        expect(docs_url(source)).to eq(expected_url)
       end
     end
   end
